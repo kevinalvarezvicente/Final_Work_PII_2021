bc7ea40b3820b0aba9e85536246d7844fac60f1e
martino-lucas95 <martino.lucas95@gmail.com>
2021-10-27 09:58:35 -0300

    Agreement and IQualification

--- src/Library/Class/Agreement.cs
+++ src/Library/Class/Agreement.cs
@@ -4,5 +4,9 @@ namespace Library
 {
     public class Agreement
     {
+        Entrepreneur entrepreneur;
+        Company company;
+        Material material;
+        DateTime agreementDate;
     }
 }
