6c59175c2fbf7b372e0af0448070061e3ade21d7
martino-lucas95 <martino.lucas95@gmail.com>
2021-11-01 10:19:39 -0300

    Fix

--- PII_ProjectTemplate-master/PII_ProjectTemplate-master/src/Library/Class/ListAgreement.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Library
-{
-    public class ListAgreement
-    {
-        private List<Agreement> listAgreement();
-    }
-}
--- PII_ProjectTemplate-master/PII_ProjectTemplate-master/src/Library/Class/UsualStock.cs
+++
@@ -1,9 +0,0 @@
-
-using System;
-
-namespace Library
-{
-    public class UsualStock
-    {
-    }
-}
