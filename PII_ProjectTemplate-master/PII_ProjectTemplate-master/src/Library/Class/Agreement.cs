18a6768351e1738056761ab0c6ee7f61b155efd5
martino-lucas95 <martino.lucas95@gmail.com>
2021-11-01 10:08:53 -0300

    ProjectTemplate

---
+++ PII_ProjectTemplate-master/PII_ProjectTemplate-master/src/Library/Class/Agreement.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Library
+{
+    public class Agreement
+    {
+        Entrepreneur entrepreneur;
+        Company company;
+        Material material;
+        DateTime agreementDate;
+    }
+}
