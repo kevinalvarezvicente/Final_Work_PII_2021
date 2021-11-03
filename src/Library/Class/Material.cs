a93b739e648010462c8746d08abbcd282d8f69ae
martino-lucas95 <martino.lucas95@gmail.com>
2021-11-03 20:38:22 -0300

    New class Token

--- src/Library/Class/Material.cs
+++ src/Library/Class/Material.cs
@@ -1,9 +1,10 @@
 using Library;
+using Ucu.Poo.Locations.Client;
 namespace Library
 {
     public class Material
     {
-        public Material (int idMaterial, int stock, string unit, double cost, Location ubication )
+        public Material (int idMaterial, int stock, string unit, double cost, Location location )
         { 
         
         }
