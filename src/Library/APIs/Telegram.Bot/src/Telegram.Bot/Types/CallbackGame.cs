47967c44bbc1068f9c9d06e21cd77769e72f5af4
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2021-11-03 21:16:18 -0300

    ApiTelegram

---
+++ src/Library/APIs/Telegram.Bot/src/Telegram.Bot/Types/CallbackGame.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Telegram.Bot.Types
+{
+    /// <summary>
+    /// A placeholder, currently holds no information. Use <see href="https://t.me/botfather">@BotFather</see> to set up your game.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class CallbackGame
+    {
+    }
+}
