8f567f321920fb12ebbd3f8b08c599a0d0b4bb24
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2021-11-24 08:30:53 -0300

    update

--- src/Library/Library/ServerBot.cs
+++ src/Library/Library/ServerBot.cs
@@ -1,18 +1,18 @@
 using System;
-using System.Collections.Generic;
-using Library;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using System.IO;
+using System.Text;
 
 namespace FINAL_WORK_PII_2021.src.Library.Class
 {
     public class ServerBot
     {
         public void startEngines(){
-            /// <summary>
-            /// We create an instance of the ChatBot calling the method Instance of the class TelegramBot.
-            /// </summary>
-            /// <value>Instance the TelegramBot.</value>
-            TelegramBot telegramBot = TelegramBot.Instance;
+            //Obtengo una instancia de TelegramBot
+            Chatbot telegramBot = Chatbot.Instance;
             Console.WriteLine($"Hola soy el Bot de P2, mi nombre es {telegramBot.BotName} y tengo el Identificador {telegramBot.BotId}");
 
             //Obtengo el cliente de Telegram
